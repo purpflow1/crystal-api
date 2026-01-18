236a0b162675f3eccfe5bd36a5a2be5356aada9e
purpflow1 <purpflow1@gmail.com>
2026-01-18 19:44:05 +0400

    removed in-crate inlines

--- src/debug.rs
+++ src/debug.rs
@@ -16,17 +16,14 @@ pub fn set_internal_logging_level(logging_level: LoggingLevel) {
     *LOGGING_LEVEL.write().unwrap() = logging_level
 }
 
-#[inline]
 pub(crate) fn get_logging_level() -> LoggingLevel {
     *LOGGING_LEVEL.read().unwrap()
 }
 
-#[inline]
 pub(crate) fn get_startup_time() -> Instant {
     unsafe { (*STARTUP_TIME.read().unwrap()).assume_init_read() }
 }
 
-#[inline]
 pub(crate) fn setup_startup_time() {
     *STARTUP_TIME.write().unwrap() = MaybeUninit::new(Instant::now())
 }
